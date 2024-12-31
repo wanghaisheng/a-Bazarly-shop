3280f044743c9718868b7f2a18dc067680e230f5
Md. Rahad Ullah <rahadullah10@gmail.com>
2024-12-31 16:21:34 +0600

    Enhance MegaMenuBar by adding links for All Products and All Shops; adjust styling for better spacing

--- src/components/shared/Navbar/MegaMenuBar.tsx
+++ src/components/shared/Navbar/MegaMenuBar.tsx
@@ -16,11 +16,13 @@ const MegaMenuBar = () => {
   const { data: categoriesData } = useGetAllCategoriesQuery({});
 
   return (
-    <div className="hidden md:block z-50 border-b border-opacity-40 px-4 md:px-6 lg:px-10">
+    <div className="hidden md:block z-50 border-b border-opacity-40 px-4 md:px-6 lg:px-10 py-2">
       <NavigationMenu className="w-full max-w-screen-2xl mx-auto justify-start">
         <NavigationMenuList>
           <NavigationMenuItem>
-            <NavigationMenuTrigger className="font-bold">All Categories</NavigationMenuTrigger>
+            <NavigationMenuTrigger className="font-bold">
+              All Categories
+            </NavigationMenuTrigger>
             <NavigationMenuContent>
               <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
                 {categoriesData?.data?.map((item: TCategory) => (
@@ -28,6 +30,20 @@ const MegaMenuBar = () => {
                 ))}
               </ul>
             </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link to={`/products`}>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                All Products
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link to={`/shops`}>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                All Shops
+              </NavigationMenuLink>
+            </Link>
           </NavigationMenuItem>
           {categoriesData?.data?.map((item: TCategory) => (
             <NavigationMenuItem key={item.id}>
