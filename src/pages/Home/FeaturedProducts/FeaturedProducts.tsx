64e92c1d7cf023438b933e1ec5a27678a4f8c0d8
Md. Rahad Ullah <rahadullah10@gmail.com>
2025-01-01 10:39:50 +0600

    Refactor FeaturedProducts component to simplify state management and enhance performance

--- src/pages/Home/FeaturedProducts/FeaturedProducts.tsx
+++ src/pages/Home/FeaturedProducts/FeaturedProducts.tsx
@@ -3,159 +3,62 @@ import { useGetAllProductsQuery } from "@/redux/features/product/productApi";
 import ProductCard from "./ProductCard";
 import { IProduct } from "@/types/TProduct";
 import { Skeleton } from "@/components/ui/skeleton";
-import { Button } from "@/components/ui/button";
-import { Search } from "lucide-react";
-import { Input } from "@/components/ui/input";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import { useCallback, useEffect, useRef, useState } from "react";
-import { useGetAllCategoriesQuery } from "@/redux/features/category/categoryApi";
-import { TCategory } from "@/types/TCategory";
-// import SectionHeader from "@/components/shared/sectionHeader";
 import { useAppSelector } from "@/redux/hook";
 import { selectCurrentUser } from "@/redux/features/auth/AuthSlice";
 import SectionHeading from "@/components/shared/SectionHeader";
 
 const FeaturedProducts = () => {
-  const [search, setSearch] = useState("");
-  const [category, setCategory] = useState("");
-  const [minPrice, setMinPrice] = useState(0);
-  const [maxPrice, setMaxPrice] = useState(99999999999999);
-  const [sort, setSort] = useState("asc");
-  const [page, setPage] = useState(1); // Current page
-  const limit = 4; // Items per page
-
   const user = useAppSelector(selectCurrentUser);
 
-  const observer = useRef<IntersectionObserver | null>(null);
+  // const observer = useRef<IntersectionObserver | null>(null);
 
-  const { data: categories } = useGetAllCategoriesQuery({});
-
-  const { data, isFetching, refetch } = useGetAllProductsQuery({
-    searchTerm: search,
-    category,
-    sortBy: "price",
-    sortOrder: sort,
-    minPrice,
-    maxPrice: maxPrice || 999999,
-    page,
-    limit,
+  const { data, isFetching } = useGetAllProductsQuery({
+    limit: 12,
     userEmail: user?.email,
   });
+  const products = data?.data;
 
-  // Concatenate fetched data
-  const [products, setProducts] = useState<IProduct[]>([]);
+  // // Concatenate fetched data
+  // const [products, setProducts] = useState<IProduct[]>([]);
 
-  // Update products when API data changes
-  useEffect(() => {
-    if (data?.data) {
-      setProducts((prev) => [...prev, ...data.data]);
-    }
-  }, [data?.data]);
+  // // Update products when API data changes
+  // useEffect(() => {
+  //   if (data?.data) {
+  //     setProducts((prev) => [...prev, ...data.data]);
+  //   }
+  // }, [data?.data]);
 
   // Implementation of infinite scrolling
-  const lastElementRef = useCallback(
-    (node: HTMLDivElement | null) => {
-      if (isFetching) return;
+  // const lastElementRef = useCallback(
+  //   (node: HTMLDivElement | null) => {
+  //     if (isFetching) return;
 
-      if (observer.current) observer.current.disconnect();
+  //     if (observer.current) observer.current.disconnect();
 
-      observer.current = new IntersectionObserver((entries) => {
-        // Check if the last item is visible and whether more data exists
-        if (
-          entries[0].isIntersecting &&
-          products.length < (data?.meta?.total || 0)
-        ) {
-          setPage((prevPage) => prevPage + 1); // Load the next page
-        }
-      });
+  //     observer.current = new IntersectionObserver((entries) => {
+  //       // Check if the last item is visible and whether more data exists
+  //       if (
+  //         entries[0].isIntersecting &&
+  //         products.length < (data?.meta?.total || 0)
+  //       ) {
+  //         setPage((prevPage) => prevPage + 1); // Load the next page
+  //       }
+  //     });
 
-      if (node) observer.current.observe(node);
-    },
-    [isFetching, products, data]
-  );
+  //     if (node) observer.current.observe(node);
+  //   },
+  //   [isFetching, products, data]
+  // );
 
   return (
     <div className="py-16 lg:py-24 bg-slate-50">
       <Container>
-        <div className="space-y-8">
+        <div className="space-y-8 mb-12">
           {/* section header */}
           <SectionHeading
             heading="Featured Products"
             subHeading="Shop the latest trends and unbeatable deals today!"
           />
-
-          {/* Search space */}
-          <div className="flex justify-center items-center">
-            <div
-              tabIndex={0}
-              className="flex flex-col md:flex-row gap-2 w-full max-w-xl items-center space-x-2 p-1 rounded-lg"
-            >
-              <Input
-                onChange={(e) => setSearch(e.target.value)}
-                type="search"
-                placeholder="Search here"
-              />
-              {/* select category */}
-              <div className="flex items-center gap-2">
-                <Select onValueChange={(value) => setCategory(value)}>
-                  <SelectTrigger className="w-[120px] bg-white">
-                    <SelectValue placeholder="Category" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectGroup>
-                      {categories?.data.map((item: TCategory) => (
-                        <SelectItem key={item.id} value={item?.name}>
-                          {item?.name}
-                        </SelectItem>
-                      ))}
-                    </SelectGroup>
-                  </SelectContent>
-                </Select>
-                <Button onClick={refetch} type="submit" variant={"default"}>
-                  <Search />
-                </Button>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        {/* price range and sorting */}
-        <div className="flex flex-wrap justify-center lg:justify-between items-center gap-2 my-6 mt-10">
-          <div className="flex items-center gap-2">
-            <Input
-              onChange={(e) => setMinPrice(Number(e.target.value))}
-              type="number"
-              placeholder="Min price"
-              className="max-w-36"
-            />
-            <Input
-              onChange={(e) => setMaxPrice(Number(e.target.value))}
-              type="number"
-              placeholder="Max price"
-              className="max-w-36"
-            />
-          </div>
-          {/* sorting */}
-          <Select onValueChange={(value) => setSort(value)}>
-            <SelectTrigger className="w-[180px] bg-white">
-              <SelectValue placeholder="Default Sorting" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectItem value="asc" defaultChecked>
-                  Price Low to High
-                </SelectItem>
-                <SelectItem value="desc">Price High to Low</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
         </div>
 
         {/* data mapping */}
@@ -163,7 +66,7 @@ const FeaturedProducts = () => {
           {products?.map((product: IProduct, index: number) => {
             if (products?.length === index + 1) {
               return (
-                <div key={index} ref={lastElementRef}>
+                <div key={index}>
                   <ProductCard product={product} />
                 </div>
               );
@@ -189,7 +92,7 @@ const FeaturedProducts = () => {
           </div>
         )}
         {/* not found */}
-        {products?.length < 1 && (
+        {!isFetching && products?.length < 1 && (
           <h1 className="text-center text-lg text-gray-500 my-10">
             No Data Found
           </h1>
